10dda6afb52c16ac4dae0bc201a146a6efcbaddd
sandiptorane <70702590+sandiptorane@users.noreply.github.com>
2020-12-28 11:50:28 +0530

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -10,24 +10,26 @@ import (
 	"userRepository/internal/vipers"
 )
 
-//test connection with database. if not able to connect terminate the application
-func init(){
-	_,err := database.DbConnect()
-	if err!=nil{
+func main() {
+	datastore, err := database.DbConnect()
+	if err != nil {        //if not able to connect with database then terminate the application
 		log.Fatal("not able connect with server database: application terminated")
 	}
-}
+	handler := handlers.NewHandler(datastore)
 
-func main(){
 	r := mux.NewRouter()
-	r.HandleFunc("/register", handlers.Registration).Methods("POST")
-	r.HandleFunc("/signin", handlers.SignIn).Methods("POST")
-	r.HandleFunc("/profile", token.IsAuthorized(handlers.GetProfile)).Methods("GET")
-	r.HandleFunc("/profile", token.IsAuthorized(handlers.UpdateProfile)).Methods("PUT")
-	r.HandleFunc("/github", token.IsAuthorized(handlers.Github)).Methods("GET")
-	r.HandleFunc("/task", token.IsAuthorized(handlers.AddTasks)).Methods("POST")
-	r.HandleFunc("/task", token.IsAuthorized(handlers.GetTasks)).Methods("GET")
-	r.HandleFunc("/signout", token.IsAuthorized(handlers.SignOut)).Methods("POST")
+	r.HandleFunc("/register", handler.Registration).Methods("POST")
+	r.HandleFunc("/signin", handler.SignIn).Methods("POST")
+	r.HandleFunc("/profile", token.IsAuthorized(handler.GetProfile)).Methods("GET")
+	r.HandleFunc("/profile", token.IsAuthorized(handler.UpdateProfile)).Methods("PUT")
+	r.HandleFunc("/github", token.IsAuthorized(handler.Github)).Methods("GET")
+	r.HandleFunc("/task", token.IsAuthorized(handler.AddTasks)).Methods("POST")
+	r.HandleFunc("/task", token.IsAuthorized(handler.GetTasks)).Methods("GET")
+	r.HandleFunc("/task/{id}", token.IsAuthorized(handler.GetSingleTask)).Methods("GET")
+	r.HandleFunc("/signout", token.IsAuthorized(handler.SignOut)).Methods("POST")
 	port := vipers.GetPort()
-	log.Fatal(http.ListenAndServe(port,r))
+	log.Fatal(http.ListenAndServe(port, r))
+
+	//close database
+	datastore.Db.Close()
 }
