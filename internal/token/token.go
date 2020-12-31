bbb44ba4134405c75ba7d9b99c03502fe63deea2
sandiptorane <70702590+sandiptorane@users.noreply.github.com>
2020-12-31 13:52:58 +0530

    Add files via upload

--- internal/token/token.go
+++ internal/token/token.go
@@ -20,6 +20,9 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+//Store the created token in tokenStore
+var  tokenStore = make(map[string]string)
+
 //SetToken will set the  token for signed user
 func CreateToken(userName string, w http.ResponseWriter,r *http.Request) (string, error) {
 	log.Println("set token for signed user")
@@ -37,15 +40,14 @@ func CreateToken(userName string, w http.ResponseWriter,r *http.Request) (string
 	// Create the JWT string
 	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		// If there is an error in creating the JWT return an internal server error
 		w.WriteHeader(http.StatusInternalServerError)
 		return "", err
 	}
 	bearer := "Bearer "+tokenString
 	w.Header().Set("Authorization",bearer)  //set token in Authorization Bearer Token header
 	r.Header.Set("Authorization",bearer)
+	tokenStore[tokenString]=claims.Username  //also store token to tokenStore
 	return tokenString, nil
-
 }
 
 //IsAuthorized authorise protected endpoints
@@ -61,8 +63,14 @@ func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handle
 			return
 		}
 		tokenString := tokenPart[1]  //tokenPart[1] contains actual token string
-		claims := &Claims{}
 
+		if _,found := tokenStore[tokenString];!found{  //check user token present in tokenStore
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintln(w,"Not Authorized")
+			return
+		}
+
+		claims := &Claims{}
 		// Parse the JWT string and store the result in `claims`.
 		tkn, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 			return jwtKey, nil
@@ -79,8 +87,18 @@ func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handle
 
 //ClearToken delete the token of signed user while sign out
 func ClearToken(w http.ResponseWriter,req *http.Request) {
-	w.Header().Set("Authorization","")
-	req.Header.Set("Authorization","")
+	auth := req.Header.Get("Authorization")  //jwt-token stored in Bearer Token Authorization header
+	//log.Println("auth token:",auth)
+	tokenPart := strings.Split(auth," ")
+	if len(tokenPart)!=2{
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintln(w,"Not Authorized")
+		return
+	}
+	tokenString := tokenPart[1]  //tokenPart[1] contains actual token string
+	delete(tokenStore,tokenString)  //delete tokenString from tokenStore
+	w.Header().Set("Authorization","Bearer ")
+	req.Header.Set("Authorization","Bearer ")
 	fmt.Fprintln(w, "signed out successfully")
 	log.Println("signed out successfully")
 }
@@ -96,15 +114,11 @@ func GetUserName(w http.ResponseWriter,req *http.Request) (userName string) {
 		return
 	}
 	tokenString := tokenPart[1]  //tokenPart[1] contains actual token string
-	claims := &Claims{}
 
-	// Parse the JWT string and store the result in `claims`.
-	tkn, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
-	if err != nil || !tkn.Valid {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
+	if user,found := tokenStore[tokenString];found{  //check user token present in tokenStore
+		return user
 	}
-	return claims.Username
+	w.WriteHeader(http.StatusUnauthorized)
+	fmt.Fprintln(w,"Not Authorized")
+	return
 }
